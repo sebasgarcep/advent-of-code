7b5735108160c69eb1ed73ce9c17782190a47b84
Sebastian Garrido <sebasgarcep@gmail.com>
2023-12-27 00:53:47 -0500

    Solve Day 10 Part 2

--- src/bin/2023/10/main.rs
+++ src/bin/2023/10/main.rs
@@ -15,12 +15,90 @@ pub fn main() {
 }
 
 fn first() {
-    solve();
+    let mut solver = FirstSolver::new();
+    solve(&mut solver);
 }
 
-fn second() {}
+struct FirstSolver {
+    size: i64,
+}
 
-fn solve() {
+impl FirstSolver {
+    pub fn new() -> Self {
+        return FirstSolver { size: 0 };
+    }
+}
+
+impl Solver for FirstSolver {
+    fn init_candidate(&mut self, _i: usize, _j: usize) {
+        self.size = 0;
+    }
+
+    fn consume_step(&mut self, _i: usize, _j: usize) {
+        self.size += 1;
+    }
+
+    fn get_result(&mut self) -> i64 {
+        return (self.size + 1) >> 1;
+    }
+}
+
+fn second() {
+    let mut solver = SecondSolver::new();
+    solve(&mut solver);
+}
+
+struct SecondSolver {
+    double_area: i64,
+    steps: i64,
+    previous: (i64, i64),
+}
+
+impl SecondSolver {
+    pub fn new() -> Self {
+        return SecondSolver {
+            double_area: 0,
+            steps: 0,
+            previous: (-1, -1),
+        };
+    }
+}
+
+impl Solver for SecondSolver {
+    fn init_candidate(&mut self, i: usize, j: usize) {
+        self.double_area = 0;
+        self.steps = 0;
+        self.previous = (i as i64, j as i64);
+    }
+
+    /*
+    The problem reduces to counting the number of lattice points within the polygon
+    but not on the perimeter. This can be calculated using Pick's theorem:
+    i = A - b/2 + 1, where A is the area of the polygon, b is the number of integer
+    points in the boundaty and i is the number of interior lattice points.
+     */
+    fn consume_step(&mut self, i: usize, j: usize) {
+        /* Use the shoelace trapezoid formula to calculate area. Instead of computing halfs, compute
+           double the amount and divide by half at the end. */
+        let value = ((j as i64) + self.previous.1) * ((i as i64) - self.previous.0);
+        self.double_area += value;
+        self.steps += 1;
+        self.previous = (i as i64, j as i64);
+    }
+
+    fn get_result(&mut self) -> i64 {
+        /* Use absolute value to cover for clockwise/counter-clockwise traversal of the loop. */
+        return (self.double_area >> 1).abs() - (self.steps >> 1) + 1;
+    }
+}
+
+trait Solver {
+    fn init_candidate(&mut self, i: usize, j: usize);
+    fn consume_step(&mut self, i: usize, j: usize);
+    fn get_result(&mut self) -> i64;
+}
+
+fn solve<S: Solver>(solver: &mut S) {
     let line_collection: Vec<String> = read_lines("data/2023/10/input.txt").collect();
 
     let width = line_collection[0].len();
@@ -66,9 +144,9 @@ fn solve() {
     }
 
     for (mut i, mut j, mut source) in candidates {
-        let mut size: i64 = 0;
+        solver.init_candidate(start_position.0, start_position.1);
         while map[i][j] != START && map[i][j] != GROUND {
-            size += 1;
+            solver.consume_step(i, j);
             let next = map[i][j] ^ source;
             match next {
                 WEST => {
@@ -88,12 +166,13 @@ fn solve() {
                     source = SOUTH;
                 }
                 _ => {
-                    unreachable!();
+                    break;
                 }
             };
         }
         if map[i][j] == START {
-            let result = (size + 2) >> 1;
+            solver.consume_step(i, j);
+            let result = solver.get_result();
             println!("{}", result);
             return;
         }
