fa4b37b439083713b3ed6233bdd2a3b467423142
Sebastian Garrido <sebasgarcep@gmail.com>
2023-12-26 19:43:35 -0500

    Day 9 Part 1

---
+++ src/bin/2023/09/main.rs
@@ -0,0 +1,38 @@
+extern crate lib;
+
+use lib::reader::read_lines;
+
+pub fn main() {
+    first();
+    second();
+}
+
+fn first() {
+    let line_iterator = read_lines("data/2023/09/input.txt");
+
+    let mut result: i64 = 0;
+    for line in line_iterator {
+        let mut values: Vec<i64> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+        let mut differences: Vec<i64> = vec![0; values.len()];
+        let mut extrapolated: i64 = 0;
+        for size in (2..=values.len()).rev() {
+            extrapolated += values[size - 1];
+            let mut is_constant = true;
+            for idx in 0..(size - 1) {
+                let current = values[idx + 1] - values[idx];
+                if current != 0 {
+                    is_constant = false;
+                }
+                differences[idx] = current;
+            }
+            if is_constant {
+                break;
+            }
+            (values, differences) = (differences, values);
+        }
+        result += extrapolated;
+    }
+    println!("{}", result);
+}
+
+fn second() {}
