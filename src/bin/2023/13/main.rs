c5dc24fb28e97168afa64ca4d3b4ee6d4c8dbaf6
Sebastian Garrido <sebasgarcep@gmail.com>
2023-12-28 12:16:06 -0500

    Solve Day 13 Part 2

--- src/bin/2023/13/main.rs
+++ src/bin/2023/13/main.rs
@@ -9,12 +9,14 @@ pub fn main() {
 }
 
 fn first() {
-    solve();
+    solve(0);
 }
 
-fn second() {}
+fn second() {
+    solve(1)
+}
 
-fn solve() {
+fn solve(num_errors: i64) {
     let mut line_collection = read_lines("data/2023/13/input.txt");
 
     /* Indexed patterns[n][j][i] */
@@ -41,59 +43,53 @@ fn solve() {
 
     let mut result: i64 = 0;
     for pattern in patterns.iter() {
-        result += get_result(pattern);
+        result += get_result(pattern, num_errors);
     }
     println!("{}", result);
 }
 
-fn get_result(pattern: &Vec<Vec<bool>>) -> i64 {
+fn get_result(pattern: &Vec<Vec<bool>>, num_errors: i64) -> i64 {
     for j in 1..pattern.len() {
-        if test_mirror_horizontal(pattern, j) {
+        if test_mirror_horizontal(pattern, num_errors, j) {
             return (j as i64) * 100;
         }
     }
     for i in 1..pattern[0].len() {
-        if test_mirror_vertical(pattern, i) {
+        if test_mirror_vertical(pattern, num_errors, i) {
             return i as i64;
         }
     }
     return 0;
 }
 
-fn test_mirror_horizontal(pattern: &Vec<Vec<bool>>, j: usize) -> bool {
+fn test_mirror_horizontal(pattern: &Vec<Vec<bool>>, num_errors: i64, j: usize) -> bool {
+    let mut curr_errors: i64 = 0;
     let num_rows = std::cmp::min(j, pattern.len() - j);
     for k in 0..num_rows {
-        if !equal_rows(pattern, j-k-1, j+k) {
-            return false;
+        for i in 0..pattern[0].len() {
+            if pattern[j - k - 1][i] != pattern[j + k][i] {
+                curr_errors += 1;
+                if curr_errors > num_errors {
+                    return false;
+                }
+            }
         }
     }
-    return true;
+    return num_errors == curr_errors;
 }
 
-fn test_mirror_vertical(pattern: &Vec<Vec<bool>>, i: usize) -> bool {
+fn test_mirror_vertical(pattern: &Vec<Vec<bool>>, num_errors: i64, i: usize) -> bool {
+    let mut curr_errors: i64 = 0;
     let num_cols = std::cmp::min(i, pattern[0].len() - i);
     for k in 0..num_cols {
-        if !equal_cols(pattern, i-k-1, i+k) {
-            return false;
+        for j in 0..pattern.len() {
+            if pattern[j][i - k - 1] != pattern[j][i + k] {
+                curr_errors += 1;
+                if curr_errors > num_errors {
+                    return false;
+                }
+            }
         }
     }
-    return true;
-}
-
-fn equal_rows(pattern: &Vec<Vec<bool>>, row_a: usize, row_b: usize) -> bool {
-    for i in 0..pattern[0].len() {
-        if pattern[row_a][i] != pattern[row_b][i] {
-            return false;
-        }
-    }
-    return true;
-}
-
-fn equal_cols(pattern: &Vec<Vec<bool>>, col_a: usize, col_b: usize) -> bool {
-    for j in 0..pattern.len() {
-        if pattern[j][col_a] != pattern[j][col_b] {
-            return false;
-        }
-    }
-    return true;
+    return num_errors == curr_errors;
 }
