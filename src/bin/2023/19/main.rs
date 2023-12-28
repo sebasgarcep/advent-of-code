82e49379cbcdc68c89855b15fbc231bbbfc56b89
Sebastian Garrido <sebasgarcep@gmail.com>
2023-12-27 21:23:15 -0500

    Solve Day 19 Part 2

--- src/bin/2023/19/main.rs
+++ src/bin/2023/19/main.rs
@@ -11,10 +11,164 @@ pub fn main() {
 }
 
 fn first() {
-    solve();
+    solve::<FirstSolver>();
 }
 
-fn second() {}
+fn second() {
+    solve::<SecondSolver>();
+}
+
+enum FirstSolver {}
+
+impl FirstSolver {
+    fn parse_data(line: String) -> HashMap<String, i64> {
+        return line[1..(line.len() - 1)]
+            .split(',')
+            .map(String::from)
+            .map(Self::parse_data_line)
+            .collect();
+    }
+
+    fn parse_data_line(mut line: String) -> (String, i64) {
+        let split_position = line.find('=').unwrap();
+        let value: i64 = line.split_off(split_position + 1).parse().unwrap();
+        line.truncate(line.len() - 1);
+        return (line, value);
+    }
+
+    fn execute_workflow_map(
+        data: &HashMap<String, i64>,
+        workflow_map: &HashMap<String, Workflow>,
+    ) -> bool {
+        let mut label = "in".to_owned();
+
+        loop {
+            let workflow = &workflow_map[&label];
+            match Self::execute_workflow(&data, &workflow) {
+                Decision::Workflow(next_label) => {
+                    label = next_label;
+                }
+                Decision::Accepted => {
+                    return true;
+                }
+                Decision::Rejected => {
+                    return false;
+                }
+            };
+        }
+    }
+
+    fn execute_workflow(data: &HashMap<String, i64>, workflow: &Workflow) -> Decision {
+        for check in workflow.checks.iter() {
+            if Self::calculate_check(&data, &check) {
+                return check.decision.clone();
+            }
+        }
+
+        return workflow.fallback.clone();
+    }
+
+    fn calculate_check(data: &HashMap<String, i64>, check: &Check) -> bool {
+        let var_value = data[&check.var_name];
+        return match check.operation {
+            Operation::GreaterThan => var_value > check.value,
+            Operation::LessThan => var_value < check.value,
+        };
+    }
+}
+
+impl Solver for FirstSolver {
+    fn get_result<I: Iterator<Item = String>>(line_collection: &mut I) -> i64 {
+        let workflow_map = parse_workflow_map(line_collection);
+
+        let mut result: i64 = 0;
+        while let Some(line) = line_collection.next() {
+            let data = Self::parse_data(line);
+            let accepted = Self::execute_workflow_map(&data, &workflow_map);
+            if accepted {
+                result += data.values().sum::<i64>();
+            }
+        }
+
+        return result;
+    }
+}
+
+enum SecondSolver {}
+
+impl SecondSolver {
+    fn get_result_from_workflow(
+        workflow_map: &HashMap<String, Workflow>,
+        mut range_set: HashMap<String, (i64, i64)>,
+        key: &str,
+    ) -> i64 {
+        let workflow = &workflow_map[key];
+        let mut total = 0;
+        for check in workflow.checks.iter() {
+            let current_range = range_set[&check.var_name];
+            match check.operation {
+                Operation::GreaterThan => {
+                    if current_range.1 <= check.value {
+                        continue;
+                    }
+                }
+                Operation::LessThan => {
+                    if current_range.0 >= check.value {
+                        continue;
+                    }
+                }
+            };
+            let mut next_range_set = range_set.clone();
+            match check.operation {
+                Operation::GreaterThan => {
+                    next_range_set.get_mut(&check.var_name).unwrap().0 = check.value + 1;
+                    range_set.get_mut(&check.var_name).unwrap().1 = check.value;
+                }
+                Operation::LessThan => {
+                    next_range_set.get_mut(&check.var_name).unwrap().1 = check.value - 1;
+                    range_set.get_mut(&check.var_name).unwrap().0 = check.value;
+                }
+            };
+            total += Self::get_result_from_decision(workflow_map, next_range_set, &check.decision);
+        }
+        total += Self::get_result_from_decision(workflow_map, range_set, &workflow.fallback);
+        return total;
+    }
+
+    fn get_result_from_decision(
+        workflow_map: &HashMap<String, Workflow>,
+        range_set: HashMap<String, (i64, i64)>,
+        decision: &Decision,
+    ) -> i64 {
+        return match decision {
+            Decision::Accepted => range_set
+                .values()
+                .map(|(start, end)| std::cmp::max(0, end - start + 1))
+                .product(),
+            Decision::Rejected => 0,
+            Decision::Workflow(label) => {
+                Self::get_result_from_workflow(workflow_map, range_set, label)
+            }
+        };
+    }
+}
+
+impl Solver for SecondSolver {
+    fn get_result<I: Iterator<Item = String>>(line_collection: &mut I) -> i64 {
+        let workflow_map = parse_workflow_map(line_collection);
+        let ranges: HashMap<String, (i64, i64)> = HashMap::from([
+            ("x".to_owned(), (1, 4000)),
+            ("m".to_owned(), (1, 4000)),
+            ("a".to_owned(), (1, 4000)),
+            ("s".to_owned(), (1, 4000)),
+        ]);
+        return Self::get_result_from_workflow(&workflow_map, ranges, "in");
+    }
+}
+
+trait Solver {
+    fn get_result<I: Iterator<Item = String>>(line_collection: &mut I) -> i64;
+}
 
 struct Workflow {
     checks: Vec<Check>,
@@ -40,9 +194,15 @@ enum Decision {
     Rejected,
 }
 
-fn solve() {
+fn solve<S: Solver>() {
     let mut line_collection = read_lines("data/2023/19/input.txt");
+    let result = S::get_result(&mut line_collection);
+    println!("{}", result);
+}
 
+fn parse_workflow_map<I: Iterator<Item = String>>(
+    line_collection: &mut I,
+) -> HashMap<String, Workflow> {
     let mut workflow_map: HashMap<String, Workflow> = HashMap::new();
     while let Some(line) = line_collection.next() {
         if line.is_empty() {
@@ -63,16 +223,7 @@ fn solve() {
 
         workflow_map.insert(workflow_name, workflow);
     }
-
-    let mut result: i64 = 0;
-    while let Some(line) = line_collection.next() {
-        let data = parse_data(line);
-        let accepted = execute_workflow_map(&data, &workflow_map);
-        if accepted {
-            result += data.values().sum::<i64>();
-        }
-    }
-    println!("{}", result);
+    return workflow_map;
 }
 
 fn parse_decision(label: String) -> Decision {
@@ -101,59 +252,4 @@ fn parse_check(line: String) -> Check {
         value,
         decision: parse_decision(line[(split_position + 1)..].to_owned()),
     };
-}
-
-fn parse_data(line: String) -> HashMap<String, i64> {
-    return line[1..(line.len() - 1)]
-        .split(',')
-        .map(String::from)
-        .map(parse_data_line)
-        .collect();
-}
-
-fn parse_data_line(mut line: String) -> (String, i64) {
-    let split_position = line.find('=').unwrap();
-    let value: i64 = line.split_off(split_position + 1).parse().unwrap();
-    line.truncate(line.len() - 1);
-    return (line, value);
-}
-
-fn execute_workflow_map(
-    data: &HashMap<String, i64>,
-    workflow_map: &HashMap<String, Workflow>,
-) -> bool {
-    let mut label = "in".to_owned();
-
-    loop {
-        let workflow = &workflow_map[&label];
-        match execute_workflow(&data, &workflow) {
-            Decision::Workflow(next_label) => {
-                label = next_label;
-            }
-            Decision::Accepted => {
-                return true;
-            }
-            Decision::Rejected => {
-                return false;
-            }
-        };
-    }
-}
-
-fn execute_workflow(data: &HashMap<String, i64>, workflow: &Workflow) -> Decision {
-    for check in workflow.checks.iter() {
-        if calculate_check(&data, &check) {
-            return check.decision.clone();
-        }
-    }
-
-    return workflow.fallback.clone();
-}
-
-fn calculate_check(data: &HashMap<String, i64>, check: &Check) -> bool {
-    let var_value = data[&check.var_name];
-    return match check.operation {
-        Operation::GreaterThan => var_value > check.value,
-        Operation::LessThan => var_value < check.value,
-    }
 }
