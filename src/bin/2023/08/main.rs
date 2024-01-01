563d8fc75b81a7a4cb9236042f31660e70f1906f
Sebastian Garrido <sebasgarcep@gmail.com>
2024-01-01 16:37:26 -0500

    Partial work

--- src/bin/2023/08/main.rs
+++ src/bin/2023/08/main.rs
@@ -1,5 +1,6 @@
 extern crate lib;
 
+use bit_set::BitSet;
 use itertools::Itertools;
 use lib::reader::read_lines;
 use std::collections::HashMap;
@@ -16,35 +17,38 @@ fn first() {
 enum FirstSolver {}
 
 impl Solver for FirstSolver {
-    fn get_result(graph: &Graph, instructions: &Vec<Direction>) -> usize {
+    fn get_start_positions(graph: &Graph) -> Vec<usize> {
+        let start = graph.get_index("AAA".as_bytes().try_into().unwrap());
+        return vec![start];
+    }
+
+    fn get_can_stop(graph: &Graph) -> Vec<bool> {
         let stop = graph.get_index("ZZZ".as_bytes().try_into().unwrap());
-        let mut smart_map: HashMap<usize, (usize, usize)> = HashMap::new();
-        for start in 0..graph.nodes.len() {
-            let mut num_steps = 0;
-            let mut current = start.clone();
-            for direction in instructions.iter() {
-                if current == stop {
-                    break;
-                }
-                num_steps += 1;
-                current = graph.get_next(current, *direction);
-            }
-            smart_map.insert(start.clone(), (current, num_steps));
-        }
-
-        let mut result: usize = 0;
-        let mut current = graph.get_index("AAA".as_bytes().try_into().unwrap());
-        while current != stop {
-            let (next, num_steps) = smart_map[&current];
-            result += num_steps;
-            current = next;
-        }
-
-        return result;
+        return (0..graph.nodes.len()).map(|i| i == stop).collect_vec();
     }
 }
 
-fn second() {}
+fn second() {
+    // solve::<SecondSolver>();
+}
+
+enum SecondSolver {}
+
+impl Solver for SecondSolver {
+    fn get_start_positions(graph: &Graph) -> Vec<usize> {
+        return (0..graph.nodes.len())
+            .filter(|i| graph.nodes[*i].label[2] == 'A' as u8)
+            .collect_vec();
+    }
+
+    fn get_can_stop(graph: &Graph) -> Vec<bool> {
+        return graph
+            .nodes
+            .iter()
+            .map(|node| node.label[2] == 'Z' as u8)
+            .collect_vec();
+    }
+}
 
 struct Graph {
     nodes: Vec<Node>,
@@ -115,8 +119,37 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug)]
+struct SolverMetadata {
+    next: usize,
+    stop_positions: BitSet,
+}
+
+impl SolverMetadata {
+    pub fn get_metadata(
+        graph: &Graph,
+        instructions: &Vec<Direction>,
+        can_stop: &Vec<bool>,
+        index: usize,
+    ) -> Self {
+        let mut current = index;
+        let mut stop_positions = BitSet::new();
+        for (step, direction) in instructions.iter().enumerate() {
+            if can_stop[current] {
+                stop_positions.insert(step);
+            }
+            current = graph.get_next(index, *direction);
+        }
+        return Self {
+            next: current,
+            stop_positions,
+        };
+    }
+}
+
 trait Solver {
-    fn get_result(graph: &Graph, instructions: &Vec<Direction>) -> usize;
+    fn get_start_positions(graph: &Graph) -> Vec<usize>;
+    fn get_can_stop(graph: &Graph) -> Vec<bool>;
 }
 
 fn solve<S: Solver>() {
@@ -134,7 +167,37 @@ fn solve<S: Solver>() {
 
     let _ = line_iterator.next();
     let graph = Graph::from_lines(line_iterator);
+    let can_stop = S::get_can_stop(&graph);
+    let start_positions = S::get_start_positions(&graph);
 
-    let result = S::get_result(&graph, &instructions);
+    let metadata = (0..graph.nodes.len())
+        .map(|index| SolverMetadata::get_metadata(&graph, &instructions, &can_stop, index))
+        .collect_vec();
+    let full_set = BitSet::from_iter(0..instructions.len());
+
+    let mut result: usize = 0;
+    let mut current_positions = start_positions.clone();
+    let mut curr_intersection: BitSet;
+    loop {
+        curr_intersection = current_positions.iter().map(|p| &metadata[*p]).fold(
+            full_set.clone(),
+            |mut acc, item| {
+                acc.intersect_with(&item.stop_positions);
+                acc
+            },
+        );
+
+        if !curr_intersection.is_empty() {
+            break;
+        }
+
+        for i in 0..current_positions.len() {
+            current_positions[i] = metadata[current_positions[i]].next;
+        }
+        result += instructions.len();
+    }
+
+    result += curr_intersection.iter().min().unwrap();
+
     println!("{}", result);
 }
