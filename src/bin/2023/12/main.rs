0e046488ef94c91c21d48193d603170d5049ff59
Sebastian Garrido <sebasgarcep@gmail.com>
2024-01-02 00:51:44 -0500

    Rewrite so its nicer

--- src/bin/2023/12/main.rs
+++ src/bin/2023/12/main.rs
@@ -11,7 +11,7 @@ pub fn main() {
 }
 
 fn first() {
-    solve::<FirstSolver>();
+    FirstSolver::solve();
 }
 
 enum FirstSolver {}
@@ -39,7 +39,7 @@ impl Solver for FirstSolver {
 }
 
 fn second() {
-    solve::<SecondSolver>();
+    SecondSolver::solve();
 }
 
 enum SecondSolver {}
@@ -62,183 +62,181 @@ impl Solver for SecondSolver {
 
 trait Solver {
     fn parse_line(line: String) -> (Vec<i8>, Vec<usize>);
-}
 
-fn solve<S: Solver>() {
-    let line_collection = read_lines("data/2023/12/input.txt");
+    fn solve() {
+        let line_collection = read_lines("data/2023/12/input.txt");
 
-    let mut result: usize = 0;
-    for line in line_collection {
-        let (arrangement, hints) = S::parse_line(line);
-        result += get_result(&arrangement, &hints);
+        let mut result: usize = 0;
+        for line in line_collection {
+            let (arrangement, hints) = Self::parse_line(line);
+            result += Self::get_result(&arrangement, &hints);
+        }
+
+        println!("{}", result);
     }
 
-    println!("{}", result);
-}
-
-fn get_result(arrangement: &Vec<i8>, hints: &Vec<usize>) -> usize {
-    let start_time: Instant = Instant::now();
-    let num_broken_total: usize = hints.iter().sum();
-    let num_broken_known: usize = arrangement.iter().filter(|&&x| x == 1).count();
-    let num_available = arrangement.iter().filter(|&&x| x == -1).count();
-    let num_available_broken = num_broken_total - num_broken_known;
-    let num_available_operational = num_available - num_available_broken;
-    let result = dfs(
-        arrangement,
-        hints,
-        num_available_broken,
-        num_available_operational,
-        0,
-        0,
-        0,
-    );
-    let end_time = Instant::now();
-    let elapsed_time = end_time - start_time;
-    println!("Duration: {:.2}", elapsed_time.as_secs_f64());
-    return result;
-}
-
-// Can be optimized by only doing recursion at branching points,
-// otherwise we can just keep iterating on a for loop.
-fn dfs(
-    arrangement: &Vec<i8>,
-    hints: &Vec<usize>,
-    num_available_broken: usize,
-    num_available_operational: usize,
-    pos: usize,
-    count: usize,
-    curr_hint: usize,
-) -> usize {
-    // Sanity check
-    if pos > arrangement.len() {
-        unreachable!();
-    }
-
-    if pos == arrangement.len() {
-        return dfs_edge_case(
-            hints,
-            num_available_broken,
-            num_available_operational,
-            count,
-            curr_hint,
-        );
-    }
-
-    let val = arrangement[pos];
-    if val != -1 {
-        return dfs_known_case(
+    fn get_result(arrangement: &Vec<i8>, hints: &Vec<usize>) -> usize {
+        let start_time: Instant = Instant::now();
+        let num_broken_total: usize = hints.iter().sum();
+        let num_broken_known: usize = arrangement.iter().filter(|&&x| x == 1).count();
+        let num_available = arrangement.iter().filter(|&&x| x == -1).count();
+        let num_available_broken = num_broken_total - num_broken_known;
+        let num_available_operational = num_available - num_available_broken;
+        let result = Self::dfs(
             arrangement,
             hints,
             num_available_broken,
             num_available_operational,
-            pos,
-            count,
-            curr_hint,
-            val,
-        );
-    }
-
-    return if num_available_operational > 0 {
-        dfs_known_case(
-            arrangement,
-            hints,
-            num_available_broken,
-            num_available_operational - 1,
-            pos,
-            count,
-            curr_hint,
             0,
-        )
-    } else {
-        0
-    } + if num_available_broken > 0 {
-        dfs_known_case(
-            arrangement,
-            hints,
-            num_available_broken - 1,
-            num_available_operational,
-            pos,
-            count,
-            curr_hint,
-            1,
-        )
-    } else {
-        0
-    };
-}
-
-fn dfs_edge_case(
-    hints: &Vec<usize>,
-    num_available_broken: usize,
-    num_available_operational: usize,
-    count: usize,
-    mut curr_hint: usize,
-) -> usize {
-    if count > 0 {
-        if count != hints[curr_hint] {
-            return 0;
-        }
-        curr_hint += 1;
-    }
-    return if num_available_broken == 0
-        && num_available_operational == 0
-        && curr_hint == hints.len()
-    {
-        1
-    } else {
-        0
-    };
-}
-
-fn dfs_known_case(
-    arrangement: &Vec<i8>,
-    hints: &Vec<usize>,
-    num_available_broken: usize,
-    num_available_operational: usize,
-    pos: usize,
-    count: usize,
-    curr_hint: usize,
-    val: i8,
-) -> usize {
-    if val == 1 {
-        if curr_hint >= hints.len() {
-            return 0;
-        }
-        return dfs(
-            arrangement,
-            hints,
-            num_available_broken,
-            num_available_operational,
-            pos + 1,
-            count + 1,
-            curr_hint,
+            0,
+            0,
         );
-    } else if val == 0 {
-        if count == 0 {
-            return dfs(
+        let end_time = Instant::now();
+        let elapsed_time = end_time - start_time;
+        println!("Duration: {:.2}", elapsed_time.as_secs_f64());
+        return result;
+    }
+
+    fn dfs(
+        arrangement: &Vec<i8>,
+        hints: &Vec<usize>,
+        num_available_broken: usize,
+        num_available_operational: usize,
+        pos: usize,
+        count: usize,
+        curr_hint: usize,
+    ) -> usize {
+        // Sanity check
+        if pos > arrangement.len() {
+            unreachable!();
+        }
+
+        if pos == arrangement.len() {
+            return Self::dfs_edge_case(
+                hints,
+                num_available_broken,
+                num_available_operational,
+                count,
+                curr_hint,
+            );
+        }
+
+        let val = arrangement[pos];
+        if val != -1 {
+            return Self::dfs_known_case(
+                arrangement,
+                hints,
+                num_available_broken,
+                num_available_operational,
+                pos,
+                count,
+                curr_hint,
+                val,
+            );
+        }
+
+        return if num_available_operational > 0 {
+            Self::dfs_known_case(
+                arrangement,
+                hints,
+                num_available_broken,
+                num_available_operational - 1,
+                pos,
+                count,
+                curr_hint,
+                0,
+            )
+        } else {
+            0
+        } + if num_available_broken > 0 {
+            Self::dfs_known_case(
+                arrangement,
+                hints,
+                num_available_broken - 1,
+                num_available_operational,
+                pos,
+                count,
+                curr_hint,
+                1,
+            )
+        } else {
+            0
+        };
+    }
+
+    fn dfs_edge_case(
+        hints: &Vec<usize>,
+        num_available_broken: usize,
+        num_available_operational: usize,
+        count: usize,
+        mut curr_hint: usize,
+    ) -> usize {
+        if count > 0 {
+            if count != hints[curr_hint] {
+                return 0;
+            }
+            curr_hint += 1;
+        }
+        return if num_available_broken == 0
+            && num_available_operational == 0
+            && curr_hint == hints.len()
+        {
+            1
+        } else {
+            0
+        };
+    }
+
+    fn dfs_known_case(
+        arrangement: &Vec<i8>,
+        hints: &Vec<usize>,
+        num_available_broken: usize,
+        num_available_operational: usize,
+        pos: usize,
+        count: usize,
+        curr_hint: usize,
+        val: i8,
+    ) -> usize {
+        if val == 1 {
+            if curr_hint >= hints.len() {
+                return 0;
+            }
+            return Self::dfs(
                 arrangement,
                 hints,
                 num_available_broken,
                 num_available_operational,
                 pos + 1,
-                count,
+                count + 1,
                 curr_hint,
             );
+        } else if val == 0 {
+            if count == 0 {
+                return Self::dfs(
+                    arrangement,
+                    hints,
+                    num_available_broken,
+                    num_available_operational,
+                    pos + 1,
+                    count,
+                    curr_hint,
+                );
+            }
+            if count != hints[curr_hint] {
+                return 0;
+            }
+            return Self::dfs(
+                arrangement,
+                hints,
+                num_available_broken,
+                num_available_operational,
+                pos + 1,
+                0,
+                curr_hint + 1,
+            );
+        } else {
+            // Sanity check
+            unreachable!();
         }
-        if count != hints[curr_hint] {
-            return 0;
-        }
-        return dfs(
-            arrangement,
-            hints,
-            num_available_broken,
-            num_available_operational,
-            pos + 1,
-            0,
-            curr_hint + 1,
-        );
-    } else {
-        // Sanity check
-        unreachable!();
     }
 }
