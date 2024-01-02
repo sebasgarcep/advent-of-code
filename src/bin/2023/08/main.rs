7b1ef727927adcc5223501408c7a201ec3bcaa20
Sebastian Garrido <sebasgarcep@gmail.com>
2024-01-01 23:31:27 -0500

    Got closer to the answer

--- src/bin/2023/08/main.rs
+++ src/bin/2023/08/main.rs
@@ -1,6 +1,6 @@
 extern crate lib;
+extern crate num;
 
-use bit_set::BitSet;
 use itertools::Itertools;
 use lib::reader::read_lines;
 use std::collections::HashMap;
@@ -121,39 +121,47 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug)]
-struct SolverMetadata {
-    next: usize,
-    stop_positions: BitSet,
-}
-
-impl SolverMetadata {
-    pub fn get_metadata(
-        graph: &Graph,
-        instructions: &Vec<Direction>,
-        can_stop: &Vec<bool>,
-        index: usize,
-    ) -> Self {
-        let mut current = index;
-        let mut stop_positions = BitSet::new();
-        for (step, direction) in instructions.iter().enumerate() {
-            if can_stop[current] {
-                stop_positions.insert(step);
-            }
-            current = graph.get_next(current, *direction);
-        }
-        return Self {
-            next: current,
-            stop_positions,
-        };
-    }
-}
-
 trait Solver {
     fn get_start_positions(graph: &Graph) -> Vec<usize>;
     fn get_can_stop(graph: &Graph) -> Vec<bool>;
 }
 
+#[derive(Debug)]
+struct SolverMetadata {
+    start_position: usize,
+    stop_position: usize,
+    steps: usize,
+}
+
+impl SolverMetadata {
+    pub fn from_data(
+        graph: &Graph,
+        instructions: &Vec<Direction>,
+        can_stop: &Vec<bool>,
+        start_position: usize,
+    ) -> Self {
+        let mut steps = 0;
+        let mut current_position = start_position;
+        while steps == 0 || !can_stop[current_position] {
+            let direction = instructions[steps % instructions.len()];
+            current_position = graph.get_next(current_position, direction);
+            steps += 1;
+        }
+        assert!(steps % instructions.len() == 0);
+        return SolverMetadata {
+            start_position,
+            stop_position: current_position,
+            steps,
+        };
+    }
+}
+
+/// We use the following assumptions to ensure the code is correct.
+/// 1. The number of steps between a starting/stop position and a stop position
+/// is a multiple of the number of instructions. This ensures stability when
+/// mapping nodes forward through the graph.
+/// 2. The only stop position reachable from a stop position is itself. This ensures
+/// that a simple loop exists after reaching a stop position for the first time.
 fn solve<S: Solver>() {
     let mut line_iterator = read_lines("data/2023/08/input.txt");
     let instructions = line_iterator
@@ -172,33 +180,57 @@ fn solve<S: Solver>() {
     let can_stop = S::get_can_stop(&graph);
     let start_positions = S::get_start_positions(&graph);
 
-    let metadata = (0..graph.nodes.len())
-        .map(|index| SolverMetadata::get_metadata(&graph, &instructions, &can_stop, index))
+    // Get metadata
+    let metadata = start_positions
+        .iter()
+        .map(|start_position| {
+            let md_start =
+                SolverMetadata::from_data(&graph, &instructions, &can_stop, *start_position);
+            let md_stop =
+                SolverMetadata::from_data(&graph, &instructions, &can_stop, md_start.stop_position);
+            assert!(md_stop.start_position == md_stop.stop_position);
+            return (md_start, md_stop);
+        })
         .collect_vec();
-    let full_set = BitSet::from_iter(0..instructions.len());
 
-    let mut result: usize = 0;
-    let mut current_positions = start_positions.clone();
-    let mut curr_intersection: BitSet;
-    loop {
-        curr_intersection = current_positions.iter().map(|p| &metadata[*p]).fold(
-            full_set.clone(),
-            |mut acc, item| {
-                acc.intersect_with(&item.stop_positions);
-                acc
-            },
-        );
+    let min_steps = metadata
+        .iter()
+        .map(|(md_start, _)| md_start.steps)
+        .max()
+        .unwrap();
 
-        if !curr_intersection.is_empty() {
-            break;
+    // Get Align Metadata (gets rid of first metadata)
+    let metadata = metadata
+        .into_iter()
+        .map(|(md_start, md_stop)| (md_stop.steps, min_steps % md_start.steps))
+        .collect_vec();
+
+    // LCM
+    let lcm = metadata
+        .iter()
+        .fold(1, |acc, (steps, _)| num::integer::lcm(acc, *steps));
+
+    // Align
+    let min_step_size = instructions.len();
+    let mut values = vec![min_step_size; metadata.len()];
+    let mut align_steps = min_step_size;
+    let mut step_size = min_step_size;
+    while !values
+        .iter()
+        .zip(metadata.iter())
+        .all(|(value, (_, target_value))| *value == *target_value)
+    {
+        align_steps += step_size;
+        for i in 0..values.len() {
+            let (cycle_length, target_value) = metadata[i];
+            let previous_value = values[i];
+            values[i] += step_size;
+            values[i] %= cycle_length;
+            if previous_value != target_value && values[i] == target_value {
+                step_size = num::integer::lcm(step_size, cycle_length);
+            }
         }
-
-        for i in 0..current_positions.len() {
-            current_positions[i] = metadata[current_positions[i]].next;
-        }
-        result += instructions.len();
     }
-    result += curr_intersection.iter().min().unwrap();
 
-    println!("{}", result);
+    println!("{}", min_steps + lcm - align_steps);
 }
