b7dedb238f753091285dc69371a82832dcafc9cf
Sebastian Garrido <sebasgarcep@gmail.com>
2024-01-07 14:58:47 -0500

    Rewrite to make it more flexible (?)

--- src/bin/2023/20/main.rs
+++ src/bin/2023/20/main.rs
@@ -15,247 +15,218 @@ pub fn main() {
 
 fn first() {
     let mut solver = FirstSolver::new();
-    solve(&mut solver);
+    solver.solve();
 }
 
 struct FirstSolver {
-    iters: usize,
     num_low: usize,
     num_high: usize,
+    memory: Vec<Vec<bool>>,
+    pulse_queue: VecDeque<(usize, usize, bool)>,
 }
 
 impl FirstSolver {
     pub fn new() -> Self {
         return Self {
-            iters: 0,
             num_low: 0,
             num_high: 0,
+            memory: Vec::new(),
+            pulse_queue: VecDeque::new(),
         };
     }
-}
 
-impl Solver for FirstSolver {
-    fn process_pulse(&mut self, _source: String, _destination: String, strength: bool) {
+    fn init_memory(&mut self, machine: &Machine) {
+        self.memory = machine
+            .modules
+            .iter()
+            .map(|module| match module.class {
+                ModuleClass::FlipFlop => vec![false],
+                ModuleClass::Conjunction => vec![false; module.sources.len()],
+                _ => Vec::new(),
+            })
+            .collect();
+    }
+
+    fn queue_pulse(&mut self, source: usize, destination: usize, strength: bool) {
         if strength {
             self.num_high += 1;
         } else {
             self.num_low += 1;
         }
-    }
-
-    fn get_result(&self) -> usize {
-        return self.num_low * self.num_high;
-    }
-
-    fn should_continue(&self) -> bool {
-        return self.iters < 1000;
-    }
-
-    fn pressed_button(&mut self) {
-        self.iters += 1;
-    }
-}
-
-fn second() {
-    let mut solver = SecondSolver::new();
-    solve(&mut solver);
-}
-
-struct SecondSolver {
-    iters: usize,
-    done: bool,
-}
-
-impl SecondSolver {
-    pub fn new() -> Self {
-        return Self {
-            iters: 0,
-            done: false,
-        };
-    }
-}
-
-impl Solver for SecondSolver {
-    fn process_pulse(&mut self, _source: String, destination: String, strength: bool) {
-        if !strength && destination == "rx".to_owned() {
-            self.done = true;
-        }
-    }
-
-    fn get_result(&self) -> usize {
-        return self.iters;
-    }
-
-    fn should_continue(&self) -> bool {
-        return !self.done;
-    }
-
-    fn pressed_button(&mut self) {
-        self.iters += 1;
-    }
-}
-
-trait Solver {
-    fn process_pulse(&mut self, source: String, destination: String, strength: bool);
-    fn get_result(&self) -> usize;
-    fn should_continue(&self) -> bool;
-    fn pressed_button(&mut self);
-}
-
-struct Module {
-    destinations: Vec<String>,
-    class: ModuleClass,
-}
-
-enum ModuleClass {
-    Broadcast,
-    FlipFlop(bool),
-    Conjunction(HashMap<String, bool>),
-}
-
-struct Machine {
-    pulse_queue: VecDeque<(String, String, bool)>,
-    module_map: HashMap<String, Module>,
-}
-
-impl Machine {
-    pub fn from_lines<I: Iterator<Item = String>>(line_collection: I) -> Self {
-        let mut module_map: HashMap<String, Module> = HashMap::new();
-        for line in line_collection {
-            let (name, module) = parse_line(line);
-            module_map.insert(name, module);
-        }
-        let conjunction_inserts = module_map
-            .iter()
-            .flat_map(|(name, module)| {
-                module
-                    .destinations
-                    .iter()
-                    .map(|destination| (name.clone(), destination.clone()))
-            })
-            .collect_vec();
-        for (name, destination) in conjunction_inserts {
-            let maybe_module = module_map.get_mut(&destination);
-            if maybe_module.is_none() {
-                continue;
-            }
-            let module = maybe_module.unwrap();
-            if let ModuleClass::Conjunction(ref mut memory) = module.class {
-                memory.insert(name, false);
-            }
-        }
-
-        return Self {
-            pulse_queue: VecDeque::new(),
-            module_map,
-        };
-    }
-
-    pub fn press_button<S: Solver>(&mut self, solver: &mut S) {
-        solver.pressed_button();
-        self.queue_pulse(solver, "button".to_owned(), "broadcaster".to_owned(), false);
-        while let Some(pulse) = self.pulse_queue.pop_front() {
-            let (source, destination, input) = pulse;
-            let output = {
-                let maybe_module: Option<&mut Module> = self.module_map.get_mut(&destination);
-                if maybe_module.is_none() {
-                    continue;
-                }
-                let module = maybe_module.unwrap();
-                let output = module.get_pulse_strength(&source, input);
-                if output.is_none() {
-                    continue;
-                }
-                output.unwrap()
-            };
-            let module = &self.module_map[&destination];
-            for item in module.destinations.iter().map(String::from).collect_vec() {
-                self.queue_pulse(solver, destination.clone(), item, output);
-            }
-        }
-    }
-
-    fn queue_pulse<S: Solver>(
-        &mut self,
-        solver: &mut S,
-        source: String,
-        destination: String,
-        strength: bool,
-    ) {
-        solver.process_pulse(source.clone(), destination.clone(), strength);
         self.pulse_queue.push_back((source, destination, strength));
     }
-}
 
-impl Module {
-    fn get_pulse_strength(&mut self, source: &str, input: bool) -> Option<bool> {
-        return match self.class {
+    fn get_pulse_strength(
+        &mut self,
+        machine: &Machine,
+        source: usize,
+        destination: usize,
+        input: bool,
+    ) -> Option<bool> {
+        let ref module = machine.modules[destination];
+        let ref mut destination_memory = self.memory[destination];
+        return match module.class {
             ModuleClass::Broadcast => Option::Some(input),
-            ModuleClass::FlipFlop(ref mut state) => {
+            ModuleClass::FlipFlop => {
                 if input {
                     return Option::None;
                 }
-                *state = !*state;
-                return Option::Some(*state);
+                destination_memory[0] = !destination_memory[0];
+                return Option::Some(destination_memory[0]);
             }
-            /* Need to do some refactoring to make this work!! */
-            ModuleClass::Conjunction(ref mut memory) => {
-                *memory.get_mut(source).unwrap() = input;
-                return Option::Some(!memory.values().all(|x| *x));
+            ModuleClass::Conjunction => {
+                let source_memory_index = module
+                    .sources
+                    .iter()
+                    .find_position(|iter_source| source == **iter_source)
+                    .unwrap().0;
+                destination_memory[source_memory_index] = input;
+                return Option::Some(!destination_memory.iter().all(|x| *x));
             }
+            _ => Option::None,
         };
     }
 }
 
-fn solve<S: Solver>(solver: &mut S) {
-    let line_collection = read_lines("data/2023/20/input.txt");
-    let mut machine = Machine::from_lines(line_collection);
-
-    while solver.should_continue() {
-        machine.press_button(solver);
+impl Solver for FirstSolver {
+    fn get_result(&mut self, machine: Machine) -> usize {
+        self.init_memory(&machine);
+        let button_index = machine.index_map["button"];
+        let broadcaster_index = machine.index_map["broadcaster"];
+        for _ in 0..1000 {
+            // Press button
+            self.queue_pulse(button_index, broadcaster_index, false);
+            // Process pulses
+            while let Some(pulse) = self.pulse_queue.pop_front() {
+                let (source, destination, input) = pulse;
+                if let Option::Some(output) =
+                    self.get_pulse_strength(&machine, source, destination, input)
+                {
+                    let ref module = machine.modules[destination];
+                    for item in module.destinations.iter() {
+                        self.queue_pulse(destination, *item, output);
+                    }
+                }
+            }
+        }
+        return self.num_low * self.num_high;
     }
-    let result = solver.get_result();
-    println!("{}", result);
 }
 
-fn parse_line(mut line: String) -> (String, Module) {
-    let split_position = line.find(" -> ").unwrap();
-    let destinations = line
-        .split_off(split_position + 4)
-        .split(',')
-        .map(|s| s.trim())
-        .map(String::from)
-        .collect_vec();
+fn second() {}
 
-    let _ = line.split_off(split_position);
-    if line == "broadcaster" {
-        return (
-            line,
-            Module {
-                destinations,
-                class: ModuleClass::Broadcast,
-            },
-        );
+#[derive(Debug)]
+struct Machine {
+    modules: Vec<Module>,
+    index_map: HashMap<String, usize>,
+}
+
+#[derive(Debug)]
+struct Module {
+    sources: Vec<usize>,
+    destinations: Vec<usize>,
+    class: ModuleClass,
+}
+
+#[derive(Debug)]
+enum ModuleClass {
+    Noop,
+    Button,
+    Broadcast,
+    FlipFlop,
+    Conjunction,
+}
+
+trait Solver {
+    fn get_result(&mut self, machine: Machine) -> usize;
+
+    fn solve(&mut self) {
+        let machine = Self::get_machine();
+        let result = self.get_result(machine);
+        println!("{}", result);
     }
 
-    let name = line.split_off(1);
-    if line == "%" {
-        return (
-            name,
-            Module {
-                destinations,
-                class: ModuleClass::FlipFlop(false),
-            },
+    fn get_machine() -> Machine {
+        let line_collection = read_lines("data/2023/20/input.txt");
+        let mut machine = Machine {
+            modules: vec![],
+            index_map: HashMap::new(),
+        };
+        Self::add_machine_spec(
+            &mut machine,
+            "button".to_owned(),
+            ModuleClass::Button,
+            vec!["broadcaster".to_owned()],
         );
-    } else if line == "&" {
-        return (
-            name,
-            Module {
-                destinations,
-                class: ModuleClass::Conjunction(HashMap::new()),
-            },
-        );
+        for line in line_collection {
+            let (source, class, destinations) = Self::parse_line(line);
+            Self::add_machine_spec(&mut machine, source, class, destinations);
+        }
+        for module in machine.modules.iter_mut() {
+            module.sources.sort();
+            module.destinations.sort();
+        }
+        return machine;
     }
 
-    unreachable!();
+    fn add_machine_spec(
+        machine: &mut Machine,
+        source: String,
+        class: ModuleClass,
+        destinations: Vec<String>,
+    ) {
+        let source_index = Self::get_label_index(machine, source);
+        let destination_indexes = destinations
+            .into_iter()
+            .map(|label| Self::get_label_index(machine, label))
+            .collect_vec();
+        machine.modules[source_index].class = class;
+        for destination_index in destination_indexes.iter() {
+            machine.modules[*destination_index]
+                .sources
+                .push(source_index);
+        }
+        machine.modules[source_index].destinations = destination_indexes;
+    }
+
+    fn get_label_index(machine: &mut Machine, label: String) -> usize {
+        if machine.index_map.contains_key(&label) {
+            return machine.index_map[&label];
+        }
+
+        let index = machine.modules.len();
+        machine.modules.push(Module {
+            class: ModuleClass::Noop,
+            sources: vec![],
+            destinations: vec![],
+        });
+        machine.index_map.insert(label, index);
+        return index;
+    }
+
+    fn parse_line(mut line: String) -> (String, ModuleClass, Vec<String>) {
+        let split_position = line.find(" -> ").unwrap();
+        let destinations = line
+            .split_off(split_position + 4)
+            .split(',')
+            .map(|s| s.trim())
+            .map(String::from)
+            .collect_vec();
+
+        let _ = line.split_off(split_position);
+        if line == "broadcaster" {
+            return (line, ModuleClass::Broadcast, destinations);
+        }
+
+        let name = line.split_off(1);
+        if line == "%" {
+            return (name, ModuleClass::FlipFlop, destinations);
+        }
+
+        if line == "&" {
+            return (name, ModuleClass::Conjunction, destinations);
+        }
+
+        unreachable!()
+    }
 }
